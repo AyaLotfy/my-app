bb7e3485770df21ac744fbb012e4a823d795fe70
AyaLotfy <ayalotfy2019@gmail.com>
2024-10-27 17:02:48 +0200

    Initial commit

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -3,7 +3,7 @@ import { Box, Typography, Avatar } from '@mui/material';
 
 const MainContent: React.FC = () => (
   <Box flex={1} display="flex" flexDirection="column" alignItems="center" justifyContent="center" p={3}>
-    <Typography variant="h5">Welcome back, John Doe</Typography>
+    <Typography variant="h5">Welcome back, Aya Lotfy</Typography>
     {/* Placeholder for a logo or loading icon */}
     <Avatar sx={{ width: 64, height: 64, mt: 3, bgcolor: 'primary.main' }}>
       {/* Use any suitable icon */}
